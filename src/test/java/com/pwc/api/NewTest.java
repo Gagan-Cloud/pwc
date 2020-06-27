cb24e5574d12708eba64361743bd277034174364
Gagan-Cloud <gagankumarjawaji@gmail.com>
2020-06-27 15:56:31 +0530

    first commit

---
+++ src/test/java/com/pwc/api/NewTest.java
@@ -0,0 +1,16 @@
+package com.pwc.api;
+				
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+
+	@Test
+	public void testEasy() {
+		Assert.assertEquals("Gagan", "Gagan");
+	}
+
+	
+
+}
